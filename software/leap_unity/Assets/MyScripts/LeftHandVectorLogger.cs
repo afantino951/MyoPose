49c16c9f174d73c3bf9261cece0e3ea1d9bb6014
afantino951 <andrew.fantino.951@gmail.com>
2024-11-16 22:16:37 -0800

    pip and mcp angles

--- software/leap_unity/Assets/MyScripts/LeftHandVectorLogger.cs
+++ software/leap_unity/Assets/MyScripts/LeftHandVectorLogger.cs
@@ -35,20 +35,7 @@ public class LeftHandVectorLogger : MonoBehaviour
 
         // Use the FingerType Enum cast to an int to select a finger from the hand
         Finger _thumb = fingers[(int)Finger.FingerType.THUMB];
-        Finger _index = fingers[(int)Finger.FingerType.INDEX];
-        Finger _middle = fingers[(int)Finger.FingerType.MIDDLE];
-        Finger _ring = fingers[(int)Finger.FingerType.RING];
-        Finger _pinky = fingers[(int)Finger.FingerType.PINKY];
 
-        // Access the bone array, then get the Metacarpal bone from it using the BoneType Enum cast to an int
-        Bone _thumbMetacarpal = _thumb.bones[(int)Bone.BoneType.METACARPAL];
-        Bone _indexMetacarpal = _index.bones[(int)Bone.BoneType.METACARPAL];
-        Bone _middleMetacarpal = _middle.bones[(int)Bone.BoneType.METACARPAL];
-        Bone _ringMetacarpal = _ring.bones[(int)Bone.BoneType.METACARPAL];
-        Bone _pinkyMetacarpal = _pinky.bones[(int)Bone.BoneType.METACARPAL];
-
-        Bone _indexProximal = _index.bones[(int)Bone.BoneType.PROXIMAL];
-        Bone _indexIntermediate = _index.bones[(int)Bone.BoneType.INTERMEDIATE];
 
         // Iterate through the other 4 fingers
         for (int i = 1; i < 5; i++) {
@@ -57,79 +44,71 @@ public class LeftHandVectorLogger : MonoBehaviour
             Bone intermediate = _hand.fingers[i].bones[(int)Bone.BoneType.INTERMEDIATE];
             Bone distal = _hand.fingers[i].bones[(int)Bone.BoneType.DISTAL];
 
-            float theta_mcp_fe = Quaternion.Angle(proximal.Rotation, metacarpal.Rotation);
-            float theta_pip = Quaternion.Angle(intermediate.Rotation, proximal.Rotation);
+            Vector3 qy = (
+                metacarpal.NextJoint
+                + proximal.Direction 
+                - Vector3.Project(proximal.Direction, metacarpal.Basis.yBasis)
+            );
+
+            float theta_mcp_aa = Vector3.SignedAngle(
+                Vector3.Normalize(qy),
+                Vector3.Normalize(metacarpal.NextJoint + metacarpal.Basis.zBasis),
+                Vector3.Normalize(metacarpal.NextJoint + metacarpal.Basis.yBasis)
+            );
+            float theta_mcp_flex = (Vector3.SignedAngle(
+                Vector3.Normalize(metacarpal.NextJoint + proximal.Direction),
+                Vector3.Normalize(metacarpal.NextJoint + metacarpal.Basis.zBasis),
+                Vector3.Normalize(metacarpal.NextJoint + metacarpal.Basis.xBasis)
+            ) * -1) + 20;
+
+            float theta_pip = Vector3.Angle(
+                Vector3.Normalize(proximal.NextJoint + intermediate.Direction),
+                Vector3.Normalize(proximal.NextJoint + proximal.Basis.zBasis)
+            );
         }
-        // Debug.Log(index_angles[0] + " " + index_angles[1] + " " + index_angles[2]);
 
-        Vector3 pv = _indexProximal.NextJoint - _indexProximal.PrevJoint;
-        Vector3 iv = _indexIntermediate.NextJoint - _indexIntermediate.PrevJoint;
-        Vector3 pi = (_indexIntermediate.Direction * _indexIntermediate.Length) - (_indexProximal.Direction * _indexProximal.Length);
-        // float index_theta_pip = Vector3.Angle(_indexProximal.Direction, pi);
-        // float index_theta_pip = Quaternion.Angle(_indexProximal.Rotation, _indexIntermediate.Rotation);
-        Vector3 index_pip_angle = _indexIntermediate.Rotation.eulerAngles - _indexProximal.Rotation.eulerAngles;
-        // Debug.Log(_hand.Rotation.eulerAngles + "\t" + _indexMetacarpal.Rotation.eulerAngles + "\t" + _indexProximal.Rotation.eulerAngles + "\t" + _indexIntermediate.Rotation.eulerAngles);
-        // Debug.Log(_indexIntermediate.Rotation.eulerAngles - _indexProximal.Rotation.eulerAngles);
+        // Finger _index = fingers[(int)Finger.FingerType.INDEX];
+        // Access the bone array, then get the Metacarpal bone from it using the BoneType Enum cast to an int
+        // Bone _indexMetacarpal = _index.bones[(int)Bone.BoneType.METACARPAL];
+        // Bone _indexProximal = _index.bones[(int)Bone.BoneType.PROXIMAL];
+        // Bone _indexIntermediate = _index.bones[(int)Bone.BoneType.INTERMEDIATE];
 
-        // float index_theta_pip = Quaternion.Angle(_indexIntermediate.Rotation, _indexProximal.Rotation);
-        float index_theta_mcp_fe = Quaternion.Angle(_indexMetacarpal.Rotation, _indexProximal.Rotation);
-        float index_theta_pip = Quaternion.Angle(_indexProximal.Rotation, _indexIntermediate.Rotation);
-        float k = (index_theta_mcp_fe / index_theta_pip);
-        // Debug.Log(index_theta_mcp_fe + "\t" + index_theta_pip + "\t" + k);
+        // Debug.DrawLine(_indexMetacarpal.NextJoint, _indexProximal.Direction, Color.red);
+        // Debug.DrawLine(_indexMetacarpal.NextJoint, _indexMetacarpal.Basis.xBasis, Color.grey);
+        // Debug.DrawLine(_indexMetacarpal.NextJoint, _indexMetacarpal.Basis.yBasis, Color.blue);
+        // Debug.DrawLine(_indexMetacarpal.NextJoint, _indexMetacarpal.Basis.zBasis, Color.green);
 
+        // Vector3 normed_x = Vector3.Normalize(_indexMetacarpal.Basis.xBasis);
+        // Vector3 normed_y = Vector3.Normalize(_indexMetacarpal.Basis.yBasis);
+        // Vector3 normed_z = Vector3.Normalize(_indexMetacarpal.Basis.zBasis);
 
-        // Theta_mcp_aa
-        const float mcp_aa_lower_limit = -15;
-        const float mcp_aa_upper_limit = 15;
+        // Vector3 qy = _indexMetacarpal.NextJoint + _indexProximal.Direction - Vector3.Project(_indexProximal.Direction, normed_y);
 
-        Debug.DrawLine(_indexMetacarpal.NextJoint, _indexProximal.Direction, Color.red);
+        // // Debug.DrawLine(_indexMetacarpal.NextJoint, qy, Color.white);
 
-        Debug.DrawLine(_indexMetacarpal.NextJoint, _indexMetacarpal.Basis.xBasis, Color.grey);
-        Debug.DrawLine(_indexMetacarpal.NextJoint, _indexMetacarpal.Basis.yBasis, Color.blue);
-        Debug.DrawLine(_indexMetacarpal.NextJoint, _indexMetacarpal.Basis.zBasis, Color.green);
+        // float mcp_aa = Vector3.SignedAngle(
+        //     Vector3.Normalize(qy),
+        //     Vector3.Normalize(_indexMetacarpal.NextJoint + _indexMetacarpal.Basis.zBasis),
+        //     Vector3.Normalize(_indexMetacarpal.NextJoint + _indexMetacarpal.Basis.yBasis)
+        // );
+        // float mcp_flex = (Vector3.SignedAngle(
+        //     Vector3.Normalize(_indexMetacarpal.NextJoint + _indexProximal.Direction),
+        //     Vector3.Normalize(_indexMetacarpal.NextJoint + normed_z),
+        //     Vector3.Normalize(_indexMetacarpal.NextJoint + normed_x)
+        // ) * -1) + 20;
+        // Debug.Log(mcp_aa + "\t" + mcp_flex);
 
+        // PIP
+        // Debug.DrawLine(_indexProximal.NextJoint, _indexIntermediate.Direction, Color.red);
         // Debug.DrawLine(_indexProximal.NextJoint, _indexProximal.Basis.xBasis, Color.grey);
         // Debug.DrawLine(_indexProximal.NextJoint, _indexProximal.Basis.yBasis, Color.blue);
         // Debug.DrawLine(_indexProximal.NextJoint, _indexProximal.Basis.zBasis, Color.green);
 
-        Vector3 normed_x = Vector3.Normalize(_indexMetacarpal.Basis.xBasis);
-        Vector3 normed_y = Vector3.Normalize(_indexMetacarpal.Basis.yBasis);
-        Vector3 normed_z = Vector3.Normalize(_indexMetacarpal.Basis.zBasis);
+        // float pip_flex = Vector3.Angle(
+        //     Vector3.Normalize(_indexProximal.NextJoint + _indexIntermediate.Direction),
+        //     Vector3.Normalize(_indexProximal.NextJoint + _indexProximal.Basis.zBasis)
+        // );
+        // Debug.Log(pip_flex);
 
-        // float vnx = Vector3.Dot(_indexProximal.Direction, normed_x);
-        // float vny = Vector3.Dot(_indexProximal.Direction, normed_y);
-        // float vnz = Vector3.Dot(_indexProximal.Direction, normed_z);
-
-        // Vector3 px = Vector3.Project(vnx * normed_x, _indexMetacarpal.Basis.xBasis);
-        // Vector3 py = Vector3.Project(vny * normed_y, _indexMetacarpal.Basis.yBasis);
-        // Vector3 pz = Vector3.Project(vnz * normed_z, _indexMetacarpal.Basis.zBasis);
-
-        // Debug.DrawRay(_indexMetacarpal.NextJoint, px, Color.magenta);
-        // Debug.DrawRay(_indexMetacarpal.NextJoint, py, Color.white);
-        // Debug.DrawRay(_indexMetacarpal.NextJoint, pz, Color.cyan);
-
-        // Vector3 qx = _indexMetacarpal.NextJoint + _indexProximal.Direction - Vector3.Project(_indexProximal.Direction, normed_x);
-        Vector3 qy = _indexMetacarpal.NextJoint + _indexProximal.Direction - Vector3.Project(_indexProximal.Direction, normed_y);
-        // Vector3 qz = _indexMetacarpal.NextJoint + _indexProximal.Direction - Vector3.Project(_indexProximal.Direction, normed_z);
-
-        // Debug.DrawRay(_indexMetacarpal.NextJoint, qx, Color.magenta);
-        Debug.DrawLine(_indexMetacarpal.NextJoint, qy, Color.white);
-        // Debug.DrawLine(_indexMetacarpal.NextJoint, qz, Color.cyan);
-
-        float abduct = Vector3.SignedAngle(
-            Vector3.Normalize(qy),
-            Vector3.Normalize(_indexMetacarpal.NextJoint + _indexMetacarpal.Basis.zBasis),
-            Vector3.Normalize(_indexMetacarpal.NextJoint + _indexMetacarpal.Basis.yBasis)
-        );
-        // abduct = Mathf.Clamp(abduct, -15, 15);
-        float flex = (Vector3.SignedAngle(
-            Vector3.Normalize(_indexMetacarpal.NextJoint + _indexProximal.Direction),
-            Vector3.Normalize(_indexMetacarpal.NextJoint + normed_z),
-            Vector3.Normalize(_indexMetacarpal.NextJoint + normed_x)
-        ) * -1) + 20;
-
-        Debug.Log(abduct + "\t" + flex);
-
-        
     }
 }
